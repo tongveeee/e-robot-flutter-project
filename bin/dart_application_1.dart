6200602a9cc165ae0e4e89203b284ed49eef46ec
tongveeee <tongvy012@gmail.com>
2022-07-17 17:07:25 +0700

    Hello

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,38 @@
+class Person {
+  // Attribute
+  final String name;
+  final String id;
+  final int? age;
+  final String major;
+
+  // Method
+  greeting() {
+    print("Hello, $name, You are $age yearold. You are studying $major");
+    // return "Hello $name";
+  }
+
+  static futureCarree() {
+    print("You will become developer");
+  }
+
+  // Constructor
+  Person({
+    required this.name,
+    required this.id,
+    this.age,
+    this.major = 'CS',
+  });
+}
+
+void main(List<String> args) {
+  Person.futureCarree();
+  
+  Person person1 = Person(
+    name: 'Darot',
+    id: '01',
+    // age: 20,
+    // major: 'CS',
+  );
+
+  person1.greeting();
+}
